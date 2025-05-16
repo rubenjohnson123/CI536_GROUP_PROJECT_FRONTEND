0ecbcc0e2d60f0f44dea115341920f1bf075b221
rubenjohnson123 <R.Johnson17@uni.brighton.ac.uk>
2025-05-16 03:45:40 +0100

    moved index.html and set vite root to .src

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,10 @@ export default defineConfig({
   build: {
     outDir: 'dist', // send build output to backend folder
     emptyOutDir: true,
-    add_item: path.resolve(__dirname, 'src/add_item_page/add_item.html'),
-    profi: path.resolve(__dirname, 'src/profi_page/prof.html'),
-    messages: path.resolve(__dirname, 'src/messages_page/messages.html'),
-    about: path.resolve(__dirname, 'src/about_page/about.html'),
-    contact: path.resolve(__dirname, 'src/contact_page/contact.html'),
-    help: path.resolve(__dirname, 'src/help_page/help.html'),
-    item_details: path.resolve(__dirname, 'src/item_details_page/item_details.html'),
-    login_page: path.resolve(__dirname, 'src/login_page/login.html'),
-    TandC: path.resolve(__dirname, 'src/TandC_page/termsandconditions.html'),
-  }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
