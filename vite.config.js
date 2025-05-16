93d86507e79f77603e073c1bcab59bbad145367a
rubenjohnson123 <R.Johnson17@uni.brighton.ac.uk>
2025-05-16 02:35:17 +0100

    Changed Dist

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   build: {
-    outDir: '../backend/dist', // send build output to backend folder
+    outDir: 'dist', // send build output to backend folder
     emptyOutDir: true,
   }
 })
