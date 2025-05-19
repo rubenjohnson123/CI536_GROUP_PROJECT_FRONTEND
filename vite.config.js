2945311efaa55324aa08bba8542d757828512033
rubenjohnson123 <R.Johnson17@uni.brighton.ac.uk>
2025-05-19 18:12:04 +0100

    changed links to fit new vite config

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 export default {
-  root: 'public', // Serve files directly from the public folder
+  root: 'public',
   build: {
-    outDir: '../dist', // Output folder (outside public)
-    emptyOutDir: true, // Clean it before building
+    outDir: '../dist',
+    emptyOutDir: true,
   },
 };
 
